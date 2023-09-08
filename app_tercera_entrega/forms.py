62aebf12972e8b38d07f98f25b5d74a164e335f9
Camilo Samson <camilosamson@outlook.com>
2023-09-08 17:43:41 -0300

    Agregadas las funcionalidades de login y register

--- app_tercera_entrega/forms.py
+++ app_tercera_entrega/forms.py
@@ -1,5 +1,9 @@
 from django import forms 
 from .models import ItemGasto, CategoriaGasto, MetaAhorro
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .views import *
+
 
 class CategoriaGastoFormulario(forms.ModelForm):
    
@@ -40,8 +44,20 @@ class MetaAhorroFormulario(forms.ModelForm):
     fecha_limite = forms.DateField(widget=MesAnioWidget)
     
 
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repita la contraseña', widget=forms.PasswordInput)
     
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
         
+        # con esto quitamos los mensajes de ayuda.
+        
+        help_texts = {k:'' for k in fields}
+    
 
         
 
--- app_tercera_entrega/urls.py
+++ app_tercera_entrega/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
+
 
 
 urlpatterns = [
@@ -11,5 +13,9 @@ urlpatterns = [
     path('busqueda-items/', busqueda_items, name='BusquedaItems' ),
     path('busqueda-items/buscar/', buscar, name='buscar'),
     path('lista-gastos/', lista_gastos, name='ListaGastos'),
+    path('login', login_request, name='Login'),
+    path('registrar', registro, name='Registro'),
+    path('logout', LogoutView.as_view(template_name='logout.html'), name='Logout'),
+    
 
 ]
